760982e1337052c2f144883c812131de8e22adf0
PauloVinic <paulo_vinicius_21@hotmail.com>
2025-08-03 22:24:11 -0300

    fix(structure): Remove arquivos duplicados da raiz do projeto

--- src/test/resources/db_load.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO estoque (id, quantidade_disponivel, criado_em, atualizado_em) VALUES (101, 50, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
-INSERT INTO estoque (id, quantidade_disponivel, criado_em, atualizado_em) VALUES (102, 10, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
-INSERT INTO estoque (id, quantidade_disponivel, criado_em, atualizado_em) VALUES (103, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
